0d865b117e7586ac6cb7569369627c461e34b83f
Laís <lais.sdsn@gmail.com>
2025-11-04 17:36:05 -0300

    todo-java: cadastra tarefas do usuario

---
+++ src/Tarefa.java
@@ -0,0 +1,57 @@
+
+import java.time.LocalDate;
+
+public class Tarefa {
+    private String titulo;
+    private String descricao;
+    private LocalDate dataEntrega;
+    private boolean concluida;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Tarefa(String titulo, String descricao, LocalDate dataEntrega) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataEntrega = dataEntrega;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataEntrega=" + dataEntrega +
+                ", concluida=" + concluida +
+                '}';
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDate dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+}
